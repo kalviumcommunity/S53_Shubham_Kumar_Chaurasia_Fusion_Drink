4735cb4be4c09defa9273aa4448fb2be4d075ada
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-04-24 12:21:24 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const dotenv = require("dotenv");
-
 const result = dotenv.config();
 
 if (result.error) {
@@ -14,6 +13,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, (error) => {
+    if (error) {
+        console.error("Error starting the server:", error);
+    } else {
+        console.log(`Server running on port ${port}`);
+    }
 });
