16bdd71b7510f867ed938d3070efcd30d53b0390
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-04-24 11:55:11 +0530

    index

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const dotenv = require("dotenv").config();
+const port = process.env.MONGO_URI || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
