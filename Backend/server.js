92a05d7369178b80e121d76694afd65c00543b9a
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-04-29 10:30:03 +0530

    POST API

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
 const cors = require('cors');
 const dotenv = require("dotenv");
 const result = dotenv.config();
@@ -29,9 +30,30 @@ app.get('/api/cocktails/:name', (req, res) => {
   }
 });
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to Backend server of Fusion Drink');
-})
+const saveMockDataToFile = (mockData) => {
+  fs.writeFile('./mockdata.json', JSON.stringify(mockData, null, 2), (err) => {
+      if (err) {
+          console.error('Error writing to mockdata.json:', err);
+      } else {
+          console.log('Mock data saved to mockdata.json');
+      }
+  });
+};
+
+app.post('/api/cocktails', (req, res) => {
+  const newCocktail = req.body;
+  if (!newCocktail || !newCocktail.name || !newCocktail.about || !newCocktail.preparationTime || !newCocktail.ingredients || !newCocktail.steps_to_prepare) {
+    res.status(400).json({ message: 'Please provide all required fields for the new cocktail.' });
+  } else {
+    mockData.push(newCocktail);
+    saveMockDataToFile(mockData); 
+    res.status(201).json({ message: 'Cocktail added successfully.', newCocktail });
+  }
+});
+
+// app.get('/',(req,res)=>{
+//     res.send('Welcome to Backend server of Fusion Drink');
+// })
 
 app.listen(port, (error) => {
     if (error) {
