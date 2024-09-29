eded6c820a666a822f4841058be4a8d26d5c4d45
Shubham <shubhamkumar081908@gmail.com>
2024-09-29 12:16:04 +0530

    systematic-way

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import './App.css'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import './App.css';
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,12 +12,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ClerkProvider>
     </BrowserRouter>
   </React.StrictMode>
